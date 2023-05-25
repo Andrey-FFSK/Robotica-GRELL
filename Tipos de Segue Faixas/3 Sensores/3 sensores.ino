11039fe77ba36003c2c0ddbfd5d866fb2e810353
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-05-25 14:51:03 -0300

    Update 3 sensores.ino
    mudanças

--- Tipos de Segue Faixas/3 Sensores/3 sensores.ino
+++ Tipos de Segue Faixas/3 Sensores/3 sensores.ino
@@ -1,3 +1,8 @@
+/* Incluindo as bibliotecas necessarias para o Oled
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+*/
 // Definindo as portas dos sensores e da portas H
 #define s_oeste 8    // rosa, OUT1 
 #define s_noroeste 10 // amarelo, OUT2
@@ -12,10 +17,11 @@
 #define mot_in4 5 // preto, esquerda, tras
 
 // Usando array para colocar todos os pinos, coloquei os sensores invertido por causa do BitSwift em baixo
-int pinos[] = {12, 11, 10, 8, 13, 9, 6, 3, 5};
+int pinos[] = {8, 10, 11, 12, 13, 9, 6, 3, 5};
 
-// Definindo variaveis
-int o = 200;
+/* Configurando o display, criando classe chamado dis com as especificações do display
+Adafruit_SSD1306 dis(128, 64, &Wire, -1);
+*/
 
 void setup()
 {
@@ -24,7 +30,14 @@ void setup()
     pinMode(pinos[i], INPUT);
   for (int i = 5; i < 9; i++)
     pinMode(pinos[i], OUTPUT);
-  Serial.begin(9600);
+    Serial.begin(9600);
+/*
+  dis.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Inicio de qualquer display, o medelo do Oled é SSD1306, "0x3c" é inmudavel
+  dis.clearDisplay();                    // Limpar o display, tambem inicio de qualquer display
+  dis.setTextSize(1);                    // Colocando a fonte do tamanho
+  dis.setTextColor(WHITE);               // Colocando a cor do texto
+  dis.setCursor(0, 0);                   // Escolhendo onde usar o cursor (quando dar clear ele coloca automaticamente no 0,0)
+ */
 }
 
 // Inicio das funções, para cada caso, totalizando 6 funções diferente
@@ -62,100 +75,173 @@ void mot2_par() // Função para o motor da direita ficar parado
 
 void loop()
 {
-
+  /*
+  dis.print("O\tNO\tN\tNE\tE");
+  for (int i = 0; i < 5; i++)
+  {
+    dis.print(pinos[i]);
+    dis.print("\t");
+  }
+  dis.display();
+  */
+  
   byte leitura = 0; // Definir sempre 0 quando definir algo como o for abaixo
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 5; i++)
     leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico
-  leitura = (~leitura) & 0b00000111;       // Colocando um inversor para que funcione com a tabela da verdade, com uma mascara para ir so os bits que eu quero
+  leitura = (~leitura) & (0b00011111); // Colocando um inversor para que funcione com a tabela da verdade, AND uma mascara para ir so os bits que eu quero
   Serial.println(leitura, BIN);
 
-  // Condições que usa a tabela da verdade
-  if (leitura == 0b000) // Condição 1, FRENTE
+  // Condições que usa a tabela da verdade, consultar para ver
+  if (leitura == 0b00000) // Condição 1
   {
-    mot1_hor(o);
-    mot2_hor(o);
+    mot1_hor(100);
+    mot2_hor(100);
   }
-  else if (leitura == 0b001) // Condição 2, VIRAR DIREITA
+  else if (leitura == 0b00001) // Condição 2
   {
-    mot1_hor(o);
-    mot2_anti(o);
-
-    /*
-    while (s_noroeste == 1)
-    {
-      mot1_hor(o);
-      mot2_anti(o);
-    }
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b00010) // Condição 3
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b00011) // Condição 4
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b00100) // Condição 5
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b00101) // Condição 6
+  {
+  }
+  else if (leitura == 0b00110) // Condição 7
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b00111) // Condição 8
+  {
+    mot1_hor(100); // Ver. frente
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01000) // Condição 9
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01001) // Condição 10
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01010) // Condição 11
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01011) // Condição 12
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b01100) // Condição 13
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01101) // Condição 14
+  {
+  }
+  else if (leitura == 0b01110) // Condição 15
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b01111) // Condição 16
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b10000) // Condição 17
+  {
+    mot1_anti(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b10001) // Condição 18
+  {
+    mot1_anti(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b10010) // Condição 19
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b10011) // Condição 20
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b10100) // Condição 21
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b10101) // Condição 22
+  {
+    mot1_hor(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b10110) // Condição 23
+  {
+  }
+  else if (leitura == 0b10111) // Condição 24
+  {
+    mot1_hor(100);
+    mot2_anti(100);
+  }
+  else if (leitura == 0b11000) // Condição 25
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b11001) // Condição 26
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b11010) // Condição 27
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b11011) // Condição 28
+  {
+  }
+  else if (leitura == 0b11100) // Condição 29
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b11101) // Condição 30
+  {
+    mot1_anti(100);
+    mot2_hor(100);
+  }
+  else if (leitura == 0b11110) // Condição 31
+  {
+  }
+  else if (leitura == 0b11111) // Condição 32
+  {
     mot1_par();
     mot2_par();
-    */
   }
-
-  else if (leitura == 0b010) // Condição 3, FRENTE
-  {
-    mot1_hor(o);
-    mot2_hor(o);
-  }
-
-  else if (leitura == 0b011) // Condição 4, VIRAR DIREITA
-  {
-    mot1_hor(o);
-    mot2_anti(o);
-
-    /*
-    while (s_noroeste == 1)
-    {
-      mot1_hor(o);
-      mot2_anti(o);
-    }
-    mot1_par();
-    mot2_par();
-    */
-  }
-
-  else if (leitura == 0b100) // Condição 5, VIRAR ESQUERDA
-  {
-    mot1_anti(o);
-    mot2_hor(o);
-
-    /*
-    while (s_nordeste == 1)
-    {
-      mot1_anti(o);
-      mot2_hor(o);
-    }
-    mot1_par();
-    mot2_par();
-    */
-  }
-
-  else if (leitura == 0b101) // Condição 6,
-  {
-  }
-
-  else if (leitura == 0b110) // Condição 7, VIRAR ESQUERDA
-  {
-    mot1_anti(o);
-    mot2_hor(o);
-
-    /*
-    while (s_nordeste == 1)
-    {
-      mot1_anti(o);
-      mot2_hor(o);
-    }
-    mot1_par();
-    mot2_par();
-    */
-  }
-
-  else // Condição 8,
-  {
-    mot1_par();
-    mot2_par();
-  }
-  // mot1_par();
-  // mot2_par();
 }
 
 /***********************************************
@@ -165,3 +251,31 @@ Tem que ajeitar a tabela da verdade
 
 https://circuitdigest.com/microcontroller-projects/arduino-uno-line-follower-robot
 ***********************************************/
+
+/***************************
+int che_d(int vel0, int vel1, int temp)
+{
+bool res = false;
+byte leitura;
+delay(500);
+mot_hor(vel0);
+mot_anti(vel1);
+delay(temp);
+mot_par();
+mot_par();
+for(int i = 0; i < 5; i++) leitura |= digitalRead(pinos[i]) << i;
+return leitura;
+res = !res;
+mot_anti(vel0);
+mot_hor(vel1);
+delay(temp);
+}
+
+byte leitura;
+bool res;
+if(res = false)
+{
+  byte leitura = 0; //Definir sempre 0 quando definir algo como o for abaixo
+for(int i = 0; i < 5; i++) leitura |= digitalRead(pinos[i]) << i; //Colocando as entrada da tabela da verdade usando um bitshift automatico
+}
+******************************/
