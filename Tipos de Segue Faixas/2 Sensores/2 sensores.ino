b4e0a884f4247fe0bca1b67690e54aa80e7caf7d
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-05-25 10:18:09 -0300

    Update 2 sensores.ino
    atualizando os mot in

--- Tipos de Segue Faixas/2 Sensores/2 sensores.ino
+++ Tipos de Segue Faixas/2 Sensores/2 sensores.ino
@@ -6,10 +6,10 @@
 #define s_leste 13    // marrom, OUT5
 
 // Motor 1 = esquerda; Motor 2 = direita
-#define mot_in1 9  // preto, esquerda, tras
-#define mot_in2 6  // branco, esquerda, frente 
-#define mot_in3 3  // cinza, direita, frente
-#define mot_in4 5 // roxo, direita, tras MAL CONTATO 
+#define mot_in1 9  // roxo, direita, tras
+#define mot_in2 6  // cinza, direita, frente 
+#define mot_in3 3  // branco, esquerda, frente
+#define mot_in4 5 // preto, esquerda, tras
 
 // Usando array para colocar todos os pinos, coloquei os sensores invertido por causa do BitSwift em baixo
 int pinos[] = {10, 12, 8, 11, 13, 9, 6, 3, 5};
@@ -30,34 +30,34 @@ void setup()
 // Inicio das funções, para cada caso, totalizando 6 funções diferente
 void mot1_anti(int velo) // Função para o motor da esquerda girar no sentido anti horario com a velocidade variavel
 {
-  analogWrite(mot_in1, velo);
-  analogWrite(mot_in2, 0);
-}
-void mot1_hor(int velo) // Função para o motor da esquerda girar no sentido horario com a velocidade variavel
-{
-  analogWrite(mot_in1, 0);
-  analogWrite(mot_in2, velo);
-}
-void mot1_par() // Função para o motor da esquerda ficar parado
-{
-  analogWrite(mot_in1, 0);
-  analogWrite(mot_in2, 0);
-}
-
-void mot2_anti(int velo) // Função para o motor da direita girar no sentido anti horario com a velocidade variavel
-{
   analogWrite(mot_in3, 0);
   analogWrite(mot_in4, velo);
 }
-void mot2_hor(int velo) // Função para o motor da direita girar no sentido horario com a velocidade variavel
+void mot1_hor(int velo) // Função para o motor da esquerda girar no sentido horario com a velocidade variavel
 {
   analogWrite(mot_in3, velo);
   analogWrite(mot_in4, 0);
 }
-void mot2_par() // Função para o motor da direita ficar parado
+void mot1_par() // Função para o motor da esquerda ficar parado
 {
   analogWrite(mot_in3, 0);
   analogWrite(mot_in4, 0);
+}
+
+void mot2_anti(int velo) // Função para o motor da direita girar no sentido anti horario com a velocidade variavel
+{
+  analogWrite(mot_in1, velo);
+  analogWrite(mot_in2, 0);
+}
+void mot2_hor(int velo) // Função para o motor da direita girar no sentido horario com a velocidade variavel
+{
+  analogWrite(mot_in1, 0);
+  analogWrite(mot_in2, velo);
+}
+void mot2_par() // Função para o motor da direita ficar parado
+{
+  analogWrite(mot_in1, 0);
+  analogWrite(mot_in2, 0);
 }
 
 void loop()
