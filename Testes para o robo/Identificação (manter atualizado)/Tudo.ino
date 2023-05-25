dee7b8ad1cba10589dee0a2c143d47557a6c2ee2
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-05-25 10:38:11 -0300

    Update Tudo.ino
    Colocando os motores para testar

--- Testes para o robo/Identificação (manter atualizado)/Tudo.ino
+++ Testes para o robo/Identificação (manter atualizado)/Tudo.ino
@@ -1,15 +1,78 @@
-int pins[] = {8, 10, 11, 12, 13};
+// Definindo as portas dos sensores e da portas H
+#define s_oeste 8    // rosa, OUT1 
+#define s_noroeste 10 // amarelo, OUT2
+#define s_norte 11    // azul, OUT4
+#define s_nordeste 12 // vermelho, OUT3                                 
+#define s_leste 13    // marrom, OUT5
+
+// Motor 1 = esquerda; Motor 2 = direita
+#define mot_in1 9  // roxo, direita, tras
+#define mot_in2 6  // cinza, direita, frente 
+#define mot_in3 3  // branco, esquerda, frente
+#define mot_in4 5 // preto, esquerda, tras MAL CONTATO 
+
+// Usando array para colocar todos os pinos, coloquei os sensores invertido por causa do BitSwift em baixo
+int pinos[] = {8, 10, 11, 12, 13, 9, 6, 3, 5};
+
+//Definindo variaveis
+int temp = 500;
+int ant = 0;
+int o = 100;
 
 void setup(){
   Serial.begin(9600);
-  for(int i; i < 5; i++){
-    pinMode(pins[i], INPUT);
-  }
+  for(int i; i < 5; i++) pinMode(pinos[i], INPUT);
+  for(int i = 5; i < 9; i++) pinMode(pinos[i], OUTPUT);
+  
 }
 
 void loop(){
 
-for(int i; i < 5; i++) Serial.print(digitalRead(pins[i]));
-Serial.println();
+mot1_hor(o);
+mot2_hor(o);
+
+//mot1_anti(o);
+//mot2_anti(o);
+
+if(millis() - ant >= temp){
+  ant = millis();
+byte leitura = 0;
+for(int i = 0; i < 5; i++) leitura |= digitalRead(pinos[i]) << i;
+leitura = (~leitura) & 0b00011111;
+Serial.println(leitura, BIN);
+}
   
+}
+
+// Inicio das funções, para cada caso, totalizando 6 funções diferente
+void mot1_anti(int velo) // Função para o motor da esquerda girar no sentido anti horario com a velocidade variavel
+{
+  analogWrite(mot_in3, 0);
+  analogWrite(mot_in4, velo);
+}
+void mot1_hor(int velo) // Função para o motor da esquerda girar no sentido horario com a velocidade variavel
+{
+  analogWrite(mot_in3, velo);
+  analogWrite(mot_in4, 0);
+}
+void mot1_par() // Função para o motor da esquerda ficar parado
+{
+  analogWrite(mot_in3, 0);
+  analogWrite(mot_in4, 0);
+}
+
+void mot2_anti(int velo) // Função para o motor da direita girar no sentido anti horario com a velocidade variavel
+{
+  analogWrite(mot_in1, velo);
+  analogWrite(mot_in2, 0);
+}
+void mot2_hor(int velo) // Função para o motor da direita girar no sentido horario com a velocidade variavel
+{
+  analogWrite(mot_in1, 0);
+  analogWrite(mot_in2, velo);
+}
+void mot2_par() // Função para o motor da direita ficar parado
+{
+  analogWrite(mot_in1, 0);
+  analogWrite(mot_in2, 0);
 }
