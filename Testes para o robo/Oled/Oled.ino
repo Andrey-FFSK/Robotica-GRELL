d4f53e64c0c4bf8e3a573de37f54c4a6f1da14f6
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-06-05 14:19:55 -0300

    Update Oled.ino
    att

--- Testes para o robo/Oled/Oled.ino
+++ Testes para o robo/Oled/Oled.ino
@@ -1,27 +1,19 @@
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 int pinos[] = {A0, A1, A2, A3, A4, 6, 5, 9, 10};
 int val[] = {0, 1, 1, 1, 0};
 
-Adafruit_SSD1306 dis(128, 64, &Wire, -1);
-
-int temp = 1000;
-unsigned long int ant;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup()
 {
-    dis.begin(SSD1306_SWITCHCAPVCC, 0x3D);
-    dis.clearDisplay();
-    dis.setTextSize(1);
-    dis.setTextColor(WHITE);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 }
 
 void loop()
 {
-    if(millis() - ant = temp){
-    ant = millis();
     dis.println("O-NO-N-NE-E");
     for (int i = 0; i < 5; i++)
     {
@@ -31,6 +23,4 @@ void loop()
         }
     }
     dis.display();
-    }
-    dis.clearDisplay();
 }
