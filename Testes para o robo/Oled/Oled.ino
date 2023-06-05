578bbd61a9241f582a68c36b4ef07dfc5c8c4366
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-06-05 14:34:05 -0300

    Update Oled.ino
    att

--- Testes para o robo/Oled/Oled.ino
+++ Testes para o robo/Oled/Oled.ino
@@ -2,25 +2,18 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-int pinos[] = {A0, A1, A2, A3, A4, 6, 5, 9, 10};
-int val[] = {0, 1, 1, 1, 0};
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup()
 {
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+     display.setTextColor(WHITE);
 }
 
 void loop()
 {
-    display.println("O-NO-N-NE-E");
-    for (int i = 0; i < 5; i++)
-    {
-        display.print(val[i]);
-        if(i < 4){
-          display.print("-");
-        }
-    }
+    display.print(F("O-NO-N-NE-E"));
     display.display();
 }
