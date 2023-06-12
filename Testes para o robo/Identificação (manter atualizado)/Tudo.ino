7493d330b7172ec81e2fea47fcef11ec77526842
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-06-12 11:03:44 -0300

    Update Tudo.ino
    led

--- Testes para o robo/Identificação (manter atualizado)/Tudo.ino
+++ Testes para o robo/Identificação (manter atualizado)/Tudo.ino
@@ -1,4 +1,11 @@
 #include <Ultrasonic.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define W 128
+#define H 64
+#define lw 6
+#define lh 8
 
 // Definindo as portas dos sensores e da portas H
 #define s_oeste 8    // rosa, OUT1 
@@ -24,7 +31,12 @@ int ant = 0;
 int o = 150;
 float tensaoA0;
 
+Adafruit_SSD1306 display(W, H, &Wire, -1);
+
 void setup(){
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
   Serial.begin(9600);
   for(int i; i < 5; i++) pinMode(pinos[i], INPUT);
   for(int i = 5; i < 9; i++) pinMode(pinos[i], OUTPUT);
@@ -32,6 +44,7 @@ void setup(){
 }
 
 void loop(){
+  display.clearDisplay();
 
 //mot1_hor(o);
 //mot2_hor(o);
@@ -39,22 +52,37 @@ void loop(){
 //mot1_anti(o);
 //mot2_anti(o);
 
-if(millis() - ant >= temp){
-ant = millis();
-Serial.print("Leitura: ");
+display.setCursor(0, lh*2);
+display.print("Leitura: ");
 byte leitura = 0;
 for(int i = 0; i < 5; i++) leitura |= digitalRead(pinos[i]) << i;
 leitura = (~leitura) & 0b00011111;
-Serial.print(leitura, BIN);
-Serial.print("Bits / ");
+for(int i = 11; i <= 15; i++) display.print(binString(leitura)[i]);
+display.print(" Bits");
+
 tensaoA0 = (div(A0) * 5) / 1024.0;
 tensaoA0 *= 8.4;
+display.setCursor(0, lh*3);
+display.print("Tensao: ");
+display.print(tensaoA0);
+display.print(" V");
+
+display.setCursor(0, lh*4);
+display.print(sensor.read());
+display.print(" cm");
+
+display.display();
+
+
+Serial.print("Leitura: ");
+Serial.print(leitura, BIN);
+Serial.print("Bits / ");
 Serial.print("Tensão: ");
 Serial.print(tensaoA0);
 Serial.print("V / ");
 Serial.print(sensor.read());
 Serial.println("cm");
-}
+
 }
 
 float div(uint8_t A0) {
@@ -97,4 +125,19 @@ void mot2_par() // Função para o motor da direita ficar parado
 {
   analogWrite(mot_in3, 0);
   analogWrite(mot_in4, 0);
+}
+
+char *binString(unsigned short n)
+{
+    static char bin[17];
+    int x;
+
+    for(x=0;x<16;x++)
+    {
+        bin[x] = n & 0x8000 ? '1' : '0';
+        n <<= 1;
+    }
+    bin[16] = '\0';
+
+    return(bin);
 }
