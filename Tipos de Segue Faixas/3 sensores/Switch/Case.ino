07f02f7e29654ec8f736056dc38ccd3339d8abb3
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-06-16 21:16:09 -0300

    Update Case.ino
    att

--- Tipos de Segue Faixas/3 sensores/Switch/Case.ino
+++ Tipos de Segue Faixas/3 sensores/Switch/Case.ino
@@ -1,4 +1,4 @@
-#include <Ultrasonic.h>
+#include <Ultrasonic.h> //Incluindo a biblioteca do ultrasonic de erik simoes
 
 // Definindo as portas dos sensores e da portas H
 #define s_oeste 8    // rosa, OUT1 
@@ -13,11 +13,11 @@
 #define mot_in3 6  // branco, direita, frente
 #define mot_in4 9 // amarelo, direita, tras
 
-// Usando array para colocar todos os pinos, coloquei os sensores invertido por causa do BitSwift em baixo
+// Usando array para colocar todos os pinos, coloquei os sensores em uma certa posição por causa do BitSwift em baixo
 int pinos[] = {11, 12, 13, 8, 13, 9, 6, 3, 5};
 
-int j = 180;
-Ultrasonic sensor(7, 4);
+const int j = 180; //PWM usado para a velocidade, min == 0 e max == 255
+Ultrasonic sensor(7, 4); //trig == 7; echo == 4
 
 void setup()
 {
@@ -61,8 +61,28 @@ void mot2_par() // Função para o motor da direita ficar parado
   analogWrite(mot_in3, 0);
   analogWrite(mot_in4, 0);
 }
-void desv_d(int velo)
+void desv_d(int velo) // Função para o robo desviar pela direita o obstaculo
 {
+  mot1_par();
+  mot2_par();
+  delay(200);
+  mot1_hor(velo);
+  mot2_anti(velo);
+  delay(800);
+  //while(digitalRead(s_norte) == 1){
+  mot1_hor(velo);
+  mot2_hor(velo);
+  delay(2100);
+  mot1_anti(velo);
+  mot2_hor(velo);
+  delay(800);
+  mot1_hor(velo);
+  mot2_hor(velo);
+  delay(2000);
+  mot1_anti(velo);
+  mot2_hor(velo);
+  delay(800);
+  /*
   byte p = 0;
   mot1_par();
   mot2_par();
@@ -80,23 +100,27 @@ void desv_d(int velo)
   p = (~p) & (0b00000111); // Colocando um inversor para que funcione com a tabela da verdade, AND uma mascara para ir so os bits que eu quero
   mot1_hor(velo-50);
   mot2_hor(velo);
-  }
+  }*/
 }
 
 void loop()
 {
+  //Essa parte é o bitSwift, criar uma variavel leitura do tipo byte, porem a gente so usa os bits dessa varaivel, a quantidade de bits depende de quantos sensores estao usando
   byte leitura = 0; // Definir sempre 0 quando definir algo como o for abaixo
   for (int i = 0; i < 3; i++)
-    leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico
-  leitura = (~leitura) & (0b00000111); // Colocando um inversor para que funcione com a tabela da verdade, AND uma mascara para ir so os bits que eu quero
+    leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico, o valor do i depende dos sensores
+  leitura = (~leitura) & (0b00000111); // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
   Serial.print(leitura, BIN);
   Serial.print(" sens: ");
   Serial.println(sensor.read());
 
-  if(sensor.read() <= 14){
+  if(sensor.read() <= 14) //Se o sensor dectar que esta 18cm de distancia ativa a função de desviar
     //desv_d(j);
-  }
 
+  //Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_nordeste e o bit mais na esquerda é o s_noroeste
+  //Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
+  //Condição de 011 ou 110: é o algoritimo de 90 graus
+    //CONCERTAR O SWITCH
 switch (leitura) {
   case (0b000) | (0b010):
     mot1_hor(j);
