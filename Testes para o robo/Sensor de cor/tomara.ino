7823b58554525ccdf46a5702a4ecbf6ba3e906ba
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-07-10 07:59:00 -0300

    Update tomara.ino
    clcand  led

--- Testes para o robo/Sensor de cor/tomara.ino
+++ Testes para o robo/Sensor de cor/tomara.ino
@@ -1,12 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #define led_r A2
-#define led_g A3
+#define led_g 8
 #define led_b A2
-#define esq A4
+#define esq A3
 #define dir A0
 
-int pin[] = {A2, A1, A3, A0, A4};
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+int pin[] = {A2, 8, A3, A0, A3};
 
 void setup(){
+
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+display.clearDisplay();
+display.setTextColor(WHITE);
+
 for(int i = 0; i < 3; i++) pinMode(pin[i], OUTPUT);
 for(int i = 3; i < 5; i++) pinMode(pin[i], INPUT);
 Serial.begin(9600);
@@ -20,6 +30,15 @@ Serial.print("Esq: ");
 Serial.print(analogRead(esq));
 Serial.print(" | Dir: ");
 Serial.println(analogRead(dir));
+
+display.clearDisplay();
+display.setCursor(0, 0);
+display.print("Esq: ");
+display.print(analogRead(esq));
+display.print(" | Dir: ");
+display.print(analogRead(dir));
+display.display();
+
 
 }
 //AZUL RILHANDO QUANDO LIGA
