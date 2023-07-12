cc7e531c13c7de933843a5c7606346de0471487e
Airam Eliel <airameliel@gmail.com>
2023-07-12 00:01:07 -0300

    atualizar

--- Tipos de Segue Faixas/4 Sensores/4 sensores.ino
+++ Tipos de Segue Faixas/4 Sensores/4 sensores.ino
@@ -1,10 +1,10 @@
 #include <Ultrasonic.h> //Incluindo a biblioteca do ultrasonic de erik simoes
 
 // Definindo as portas dos sensores e da portas H
-#define s_oeste 8    // cinza, OUT1 
+#define s_oeste 8     // cinza, OUT1
 #define s_noroeste 13 // branco, OUT2
 #define s_norte 12    // verde, OUT4
-#define s_nordeste 11 // preto, OUT3                                 
+#define s_nordeste 11 // preto, OUT3
 #define s_leste 10    // azul, OUT5
 
 // Motor 2 = esquerda; Motor 1 = direita
@@ -16,8 +16,8 @@
 // Usando array para colocar todos os pinos, coloquei os sensores em uma certa posição por causa do BitSwift em baixo
 const int pinos[] = {10, 11, 13, 8, 12, 9, 6, 3, 5};
 
-const int j = 180; //PWM usado para a velocidade, min == 0 e max == 255
-Ultrasonic sensor(7, 4); //trig == 7; echo == 4
+const int j = 180;       // PWM usado para a velocidade, min == 0 e max == 255
+Ultrasonic sensor(7, 4); // trig == 7; echo == 4
 
 void setup()
 {
@@ -26,7 +26,7 @@ void setup()
     pinMode(pinos[i], INPUT);
   for (int i = 5; i < 9; i++)
     pinMode(pinos[i], OUTPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 // Inicio das funções, para cada caso, totalizando 6 funções diferente
@@ -69,7 +69,7 @@ void desv_d(int velo) // Função para o robo desviar pela direita o obstaculo
   mot1_hor(velo);
   mot2_anti(velo);
   delay(800);
-  //while(digitalRead(s_norte) == 1){
+  // while(digitalRead(s_norte) == 1){
   mot1_hor(velo);
   mot2_hor(velo);
   delay(2100);
@@ -87,22 +87,23 @@ void desv_d(int velo) // Função para o robo desviar pela direita o obstaculo
 
 void loop()
 {
-  //Essa parte é o bitSwift, criar uma variavel leitura do tipo byte, porem a gente so usa os bits dessa varaivel, a quantidade de bits depende de quantos sensores estao usando
+  // Essa parte é o bitSwift, criar uma variavel leitura do tipo byte, porem a gente so usa os bits dessa varaivel, a quantidade de bits depende de quantos sensores estao usando
   byte leitura = 0; // Definir sempre 0 quando definir algo como o for abaixo
   for (int i = 0; i < 4; i++)
     leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico, o valor do i depende dos sensores
-  leitura = (~leitura) & (0b00001111); // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
+  leitura = (~leitura) & (0b00001111);     // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
   Serial.print(leitura, BIN);
   Serial.print(" sens: ");
   Serial.println(sensor.read());
 
-  if(sensor.read() <= 18){ //Se o sensor dectar que esta 18cm de distancia ativa a função de desviar
+  if (sensor.read() <= 18)
+  { // Se o sensor dectar que esta 18cm de distancia ativa a função de desviar
     // desv_d(j);
   }
 
-  //Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
-  //Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
-  //Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
+  // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
+  // Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
+  // Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
   if (leitura == 0b0000) // Condição 1
   {
     mot1_hor(j);
@@ -118,7 +119,8 @@ void loop()
     mot1_hor(j);
     mot2_hor(j);
     delay(200);
-    while(digitalRead(s_norte) == 1){
+    while (digitalRead(s_norte) == 1)
+    {
       mot1_hor(j);
       mot2_anti(j);
     }
@@ -127,7 +129,7 @@ void loop()
   else if ((leitura == 0b0100) /*| (leitura == 0b1000)*/) // Condição 5
   {
     mot1_anti(j);
-    mot2_hor(j); 
+    mot2_hor(j);
   }
   else if ((leitura == 0b0110) | (leitura == 0b1001)) // Condição 6
   {
@@ -139,13 +141,15 @@ void loop()
   {
     mot1_hor(j);
     mot2_hor(j);
-    delay(300);
-    while(digitalRead(s_norte) == 1){
+    delay(200);
+    while (digitalRead(s_norte) == 1)
+    {
       mot1_anti(j);
       mot2_hor(j);
     }
     delay(100);
-  } else if (leitura == 0b1111)
+  }
+  else if (leitura == 0b1111)
   {
     mot1_hor(j);
     mot2_hor(j);
--- Tipos de Segue Faixas/4 Sensores/Switch/Case.ino
+++ Tipos de Segue Faixas/4 Sensores/Switch/Case.ino
@@ -1,10 +1,10 @@
 #include <Ultrasonic.h> //Incluindo a biblioteca do ultrasonic de erik simoes
 
 // Definindo as portas dos sensores e da portas H
-#define s_oeste 8    // rosa, OUT1 
+#define s_oeste 8     // rosa, OUT1
 #define s_noroeste 13 // amarelo, OUT2
 #define s_norte 12    // azul, OUT4
-#define s_nordeste 11 // vermelho, OUT3                                 
+#define s_nordeste 11 // vermelho, OUT3
 #define s_leste 10    // marrom, OUT5
 
 // Motor 1 = esquerda; Motor 2 = direita
@@ -16,8 +16,8 @@
 // Usando array para colocar todos os pinos, coloquei os sensores em uma certa posição por causa do BitSwift em baixo
 const int pinos[] = {10, 11, 13, 8, 12, 9, 6, 3, 5};
 
-const int j = 180; //PWM usado para a velocidade, min == 0 e max == 255
-Ultrasonic sensor(7, 4); //trig == 7; echo == 4
+const int j = 130;       // PWM usado para a velocidade, min == 0 e max == 255
+Ultrasonic sensor(7, 4); // trig == 7; echo == 4
 
 void setup()
 {
@@ -26,53 +26,56 @@ void setup()
     pinMode(pinos[i], INPUT);
   for (int i = 5; i < 9; i++)
     pinMode(pinos[i], OUTPUT);
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 void loop()
 {
-  //Essa parte é o bitSwift, criar uma variavel leitura do tipo byte, porem a gente so usa os bits dessa varaivel, a quantidade de bits depende de quantos sensores estao usando
+  // Essa parte é o bitSwift, criar uma variavel leitura do tipo byte, porem a gente so usa os bits dessa varaivel, a quantidade de bits depende de quantos sensores estao usando
   byte leitura = 0; // Definir sempre 0 quando definir algo como o for abaixo
   for (int i = 0; i < 4; i++)
     leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico, o valor do i depende dos sensores
-  leitura = (~leitura) & (0b00001111); // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
+  leitura = (~leitura) & (0b00001111);     // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
   Serial.print(leitura, BIN);
   Serial.print(" sens: ");
   Serial.println(sensor.read());
 
-  if(sensor.read() <= 18) //Se o sensor dectar que esta 18cm de distancia ativa a função de desviar
-    //desv_d(j);
-
-  //Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
-  //Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
-  //Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
- switch(leitura) {
-  case 0b0000: mot1_hor(j); mot2_hor(j); break;
-  case 0b0010: mot1_hor(j);mot2_anti(j); break;
-  case 0b0011: 
-    mot1_hor(j);
-    mot2_hor(j);
-    delay(300);
-    while(digitalRead(s_norte) == 1){
-    mot1_hor(j);
-    mot2_anti(j);
+  if (sensor.read() <= 7) // desv_d(j); // Se o sensor dectar que esta 18cm de distancia ativa a função de desviar
+                           
+    // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
+    // Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
+    // Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
+    switch (leitura)
+    {
+    case 0b0000: mot1_hor(j); mot2_hor(j); break;
+    case 0b0010: mot1_hor(j); mot2_anti(j); break;
+    case 0b0011:
+      mot1_hor(j);
+      mot2_hor(j);
+      delay(200);
+      while (digitalRead(s_norte) == 1)
+      {
+        mot1_hor(j);
+        mot2_anti(j);
+      }
+      delay(100);
+      break;
+    case 0b0100: mot1_anti(j); mot2_hor(j); break;
+    case 0b0110:
+    case 0b1001: mot1_par(); mot2_par(); delay(100); break;
+    case 0b1100:
+      mot1_hor(j);
+      mot2_hor(j);
+      delay(200);
+      while (digitalRead(s_norte) == 1)
+      {
+        mot1_anti(j);
+        mot2_hor(j);
+      }
+      delay(100);
+      break;
+    case 0b1111: mot1_hor(j); mot2_hor(j); delay(300);
+    default: break;
     }
-    delay(200); break;
-  case 0b0100: mot1_anti(j); mot2_hor(j); break;
-  case 0b0110:
-  case 0b1001: mot1_par(); mot2_par(); delay(200); break;
-  case 0b1100: 
-    mot1_hor(j);
-    mot2_hor(j);
-    delay(300);
-    while(digitalRead(s_norte) == 1){
-    mot1_anti(j);
-    mot2_hor(j);
-    }
-    delay(200); break;
-   case 0b1111: mot1_hor(j); mot2_hor(j); delay(300);
- default: break;
-  
- }
 }
 
 // Inicio das funções, para cada caso, totalizando 6 funções diferente
@@ -108,7 +111,7 @@ void mot2_par() // Função para o motor da direita ficar parado
   analogWrite(mot_in2, 0);
 }
 
-//codigo de teste da entrada das condiçoes de switch
+// codigo de teste da entrada das condiçoes de switch
 /***********
   #include <iostream>
 
@@ -118,12 +121,12 @@ enum class byte : unsigned char {};
 
 int main()
 {
-    loop: 
+    loop:
     byte leitura = (byte) 0b0001;
-    
+
     cout << "-------------" << endl;
     cout << endl;
-    
+
     switch(leitura) {
         case (byte) 0b0000: cout << "condicao 1"; break;
         case (byte) 0b0001:
