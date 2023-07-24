5d2ce146a2f7b481656ab32c77b2fea832c1e249
Luck07 <airameliel@gmail.com>
2023-07-24 15:34:22 -0300

    Colocando display, e atulizando valores

--- Testes para o robo/Sensor de cor/Sensor de cor.ino
+++ Testes para o robo/Sensor de cor/Sensor de cor.ino
@@ -1,21 +1,21 @@
-/*
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-*/
+
 #define led_g 2
-#define esq A0
-#define dir A1
-/*
-Adafruit_SSD1306 display(128, 64, &Wire, -1);*/
+#define esq A1
+#define dir A0
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 int pin[] = {2, A0, A1};
 
 void setup(){
-/*
+
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 display.clearDisplay();
 display.setTextColor(WHITE);
-*/
+
 for(int i = 0; i < 1; i++) pinMode(pin[i], OUTPUT);
 for(int i = 1; i < 3; i++) pinMode(pin[i], INPUT);
 Serial.begin(9600);
@@ -23,13 +23,14 @@ Serial.begin(9600);
 
 void loop(){
   digitalWrite(led_g, 1);
-  /*
+  
   Serial.print("Esq: ");
-  Serial.print(analogRead(esq));
-  Serial.print(" Dir: ");
+  Serial.println(analogRead(esq));
+  Serial.println();
+  /*Serial.print(" Dir: ");
   Serial.println(analogRead(dir));*/
   
- int m_esq = map(constrain(analogRead(esq), 30, 150), 30, 150, 0, 1023);
+ int m_esq = map(constrain(analogRead(esq), 30, 180), 30, 180, 0, 1023);
  int m_dir = map(constrain(analogRead(dir), 30, 300), 30, 300, 0, 1023);
 Serial.print("Esq: ");
 Serial.print(m_esq);
@@ -61,14 +62,19 @@ else{
     Serial.println(" Dir = Verde");
 }*/
 
-/*
+
 display.clearDisplay();
 display.setCursor(0, 0);
 display.print("Esq: ");
 display.print(analogRead(esq));
 display.print(" | Dir: ");
-display.print(analogRead(dir));
+display.println(analogRead(dir));
+
+display.print("Esq: ");
+display.print(m_esq);
+display.print(" | Dir: "); 
+display.print(m_dir);
 display.display();
-*/
+
 
 }
