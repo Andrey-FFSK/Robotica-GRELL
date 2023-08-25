b02cd5ac80a35d370cc799ba0787833ada2b2b50
Airam Eliel <airameliel@gmail.com>
2023-08-24 21:45:28 -0300

    tentando aplicar a dica de erik

--- Tipos de Segue Faixas/4 Sensores/Sensores tomaz.ino
+++ Tipos de Segue Faixas/4 Sensores/Sensores tomaz.ino
@@ -2,18 +2,17 @@
 #include "Include.h"
 #include "Oled.h"
 
-
 // Usando array para colocar todos os pinos, coloquei os sensores em uma certa posição por causa do BitSwift em baixo
-const int pinos[] = {s_oeste, s_noroeste, s_nordeste, s_leste, s_norte, esq, dir, led_g, mot_in1, mot_in2, mot_in3, mot_in4};
+const int pinos[] = {s_oeste, s_noroeste, s_nordeste, s_leste, s_norte, esq, dir, incli, led_g, mot_in1, mot_in2, mot_in3, mot_in4};
 
 void setup()
 {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);
   // Colocando os sensores como INPUT, e o resto como OUTPUT, tudo isso pelo array
-  for (int i = 0; i < 7; i++)
+  for (int i = 0; i < 8; i++)
     pinMode(pinos[i], INPUT);
-  for (int i = 7; i < 12; i++)
+  for (int i = 8; i < 12; i++)
     pinMode(pinos[i], OUTPUT);
   Serial.begin(9600);
 }
@@ -26,7 +25,7 @@ void loop()
     leitura |= digitalRead(pinos[i]) << i; // Colocando as entrada da tabela da verdade usando um bitshift automatico, o valor do i depende dos sensores
   leitura = (~leitura) & (0b00001111);     // Colocando um inversor para que funcione com a tabela da verdade, pq o sensor dectectar no branco, AND uma mascara para ir so os bits que eu quero
 
-   //if (ult_meio.read() <= 9) desv_d(vel_esq, vel_dir); // Se o sensor dectar que esta distancia ativa a função de desviar
+  // if (ult_meio.read() <= 9) desv_d(vel_esq, vel_dir); // Se o sensor dectar que esta distancia ativa a função de desviar
 
   // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
   // Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
