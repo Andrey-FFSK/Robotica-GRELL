b8a53013710445e0c49445ee6681bdcf619083a0
Airam Eliel <32375803+Luck07@users.noreply.github.com>
2023-08-28 18:59:28 -0300

    Update Dica de erik(sala3).ino
    att

--- Tipos de Segue Faixas/4 Sensores/Dica de erik/Dica de erik(sala3).ino
+++ Tipos de Segue Faixas/4 Sensores/Dica de erik/Dica de erik(sala3).ino
@@ -70,189 +70,221 @@ void loop()
         }
     }
     if (sala3 == false)
-    {
+    {  
 
-        if (ult_meio.read() <= 9) // Se o sensor dectar que esta distancia ativa a função de desviar
-        {
-            desv_d(vel_esq, vel_dir);
-            display.setCursor(0, 0);
-            display.println("Desviando obsta");
-            display.display();
-        }
+  if (ult_meio.read() <= 4)  // Se o sensor dectar que esta distancia ativa a função de desviar
+  {
+    desv_d(vel_esq, vel_dir);
+    display.setCursor(0, 0);
+    display.println("Desviando obsta");
+    display.display();
+  }
 
-        // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
-        // Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
-        // Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
-        if (leitura == 0b0010) // Condição 2
-        {
-            mot1_hor(vel_esq);
-            mot2_anti(vel_dir);
+  // Condições que usa a melhor situação dos sensores, o bit mais da direita é o s_leste e o bit mais na esquerda é o s_oeste
+  // Algumas tem if com OR por conta que eles fazem a mesma coisa na condição.
+  // Condição de 0011 ou 1100: é o algoritimo de 90 graus, pensando que so vai ativar no 90
+  if (leitura == 0b0010)  // Condição 2
+  {
+    if (ver == false) {
+      mot1_hor(vel_esq);
+      mot2_anti(vel_dir);
 
-            display.setCursor(0, 0);
-            display.println("lei == 0010 / Direita");
-            display.display();
+      display.setCursor(0, 0);
+      display.println("lei == 0010 / Direita");
+      display.display();
 
-            Serial.println("leitura == 0010 / ajustando para direita: ");
-        }
-        else if (leitura == 0b0100) // Condição 5
-        {
-            mot1_anti(vel_esq);
-            mot2_hor(vel_dir);
+      Serial.println("leitura == 0010 / ajustando para direita: ");
+    } else {
+      display.setCursor(0, 0);
+      display.println("lei == 0010 / Tras");
+      display.display();
+      enc_ant = enc.read();
+      while (enc_ant - enc.read() <= enc_pas) {
+        mot1_anti(vel_esq);
+        mot2_anti(vel_dir);
+        Serial.print("Indo para tras: ");
+        Serial.println(enc.read());
+      }
+      ver = false;
+    }
 
-            display.setCursor(0, 0);
-            display.println("lei == 0100 / Esquerda");
-            display.display();
+  } else if (leitura == 0b0100)  // Condição 5
+  {
+    if (ver == false) {
+      mot1_anti(vel_esq);
+      mot2_hor(vel_dir);
 
-            Serial.println("leitura == 0100 / ajustando para esquerda");
-        }
-        else if ((leitura == 0b0000) || (leitura == 0b0110)) // Condição 1
-        {
-            mot1_hor(vel_esq);
-            mot2_hor(vel_dir);
+      display.setCursor(0, 0);
+      display.println("lei == 0100 / Esquerda");
+      display.display();
 
-            display.setCursor(0, 0);
-            display.println("lei = 0000");
-            display.display();
+      Serial.println("leitura == 0100 / ajustando para esquerda");
+    } else {
+      display.setCursor(0, 0);
+      display.println("lei == 0100 / Tras");
+      display.display();
+      enc_ant = enc.read();
+      while (enc_ant - enc.read() <= enc_pas) {
+        mot1_anti(vel_esq);
+        mot2_anti(vel_dir);
+        Serial.print("Indo para tras: ");
+        Serial.println(enc.read());
+      }
+      ver = false;
+    }
 
-            Serial.println("leitura = 0000; leitura == 0110");
-        }
-        else if ((leitura == 0b1000) || /*(leitura == 0b1100) ||*/ (leitura == 0b1110)) // Condição 4
-        {
-            if (ver == false)
-            {
-                digitalWrite(led_g, 1);
-                mot1_par();
-                mot2_par();
-                delay(1000);
-                sensi();
-                digitalWrite(led_g, 0);
-                ver = true;
-            }
-            else
-            {
-                ver = false;
-                Serial.print("Esq: ");
-                Serial.print(m_esq);
-                Serial.print("(");
-                Serial.print(analogRead(esq));
-                Serial.print(") / Dir: ");
-                Serial.print(m_dir);
-                Serial.print("(");
-                Serial.print(analogRead(dir));
-                Serial.println(")");
+  } else if ((leitura == 0b0000) || (leitura == 0b0110))  // Condição 1
+  {
+    if (ver == false) {
+      mot1_hor(vel_esq);
+      mot2_hor(vel_dir);
 
-                display.setCursor(0, 0);
-                display.print("Esq: ");
-                display.print(m_esq);
-                display.print("(");
-                display.print(analogRead(esq));
-                display.println(")");
+      display.setCursor(0, 0);
+      display.println("lei = 0000");
+      display.display();
 
-                display.print("Dir: ");
-                display.print(m_dir);
-                display.print("(");
-                display.print(analogRead(dir));
-                display.println(")");
+      Serial.println("leitura = 0000; leitura == 0110");
+    } else {
+      display.setCursor(0, 0);
+      display.println("lei == 0000 / Tras");
+      display.display();
+      enc_ant = enc.read();
+      while (enc_ant - enc.read() <= enc_pas) {
+        mot1_anti(vel_esq);
+        mot2_anti(vel_dir);
+        Serial.print("Indo para tras: ");
+        Serial.println(enc.read());
+      }
+      ver = false;
+    }
 
-                display.println("lei == 0b1000 / Esq_90");
-                display.display();
-                delay(3000);
-                esq_90(); // virar a direita; antes tava encruzilhada();
-            }
-        }
+  } else if ((leitura == 0b1000) || (leitura == 0b1100) || (leitura == 0b1110) || (leitura == 0b1010))  // Condição 4
+  {
+    if (ver == false) {
+      digitalWrite(led_g, 1);
+      mot1_par();
+      mot2_par();
+      delay(1000);
+      sensi();
+      digitalWrite(led_g, 0);
+      ver = true;
+    } else {
+      ver = false;
+      Serial.print("Esq: ");
+      Serial.print(m_esq);
+      Serial.print("(");
+      Serial.print(analogRead(esq));
+      Serial.print(") / Dir: ");
+      Serial.print(m_dir);
+      Serial.print("(");
+      Serial.print(analogRead(dir));
+      Serial.println(")");
 
-        else if ((leitura == 0b0001) || /*(leitura == 0b0011) ||*/ (leitura == 0b0111)) // Condição 7
-        {
-            if (ver == false)
-            {
-                digitalWrite(led_g, 1);
-                mot1_par();
-                mot2_par();
-                delay(1000);
-                sensi();
-                digitalWrite(led_g, 0);
-                ver = true;
-            }
-            else
-            {
-                ver = false;
-                Serial.print("Esq: ");
-                Serial.print(m_esq);
-                Serial.print("(");
-                Serial.print(analogRead(esq));
-                Serial.print(") / Dir: ");
-                Serial.print(m_dir);
-                Serial.print("(");
-                Serial.print(analogRead(dir));
-                Serial.println(")");
+      display.setCursor(0, 0);
+      display.print("Esq: ");
+      display.print(m_esq);
+      display.print("(");
+      display.print(analogRead(esq));
+      display.println(")");
 
-                display.setCursor(0, 0);
-                display.print("Esq: ");
-                display.print(m_esq);
-                display.print("(");
-                display.print(analogRead(esq));
-                display.println(")");
+      display.print("Dir: ");
+      display.print(m_dir);
+      display.print("(");
+      display.print(analogRead(dir));
+      display.println(")");
 
-                display.print("Dir: ");
-                display.print(m_dir);
-                display.print("(");
-                display.print(analogRead(dir));
-                display.println(")");
+      display.println("lei == 0b1000 / Esq_90");
+      display.display();
+      delay(3000);
+      esq_90();  // virar a direita; antes tava encruzilhada();
+    }
+  }
 
-                display.println("lei == 0001 / Dir_90");
-                display.display();
+  else if ((leitura == 0b0001) || (leitura == 0b0011) || (leitura == 0b0111) || (leitura == 0b0101))  // Condição 7
+  {
+    if (ver == false) {
+      digitalWrite(led_g, 1);
+      mot1_par();
+      mot2_par();
+      delay(1000);
+      sensi();
+      digitalWrite(led_g, 0);
+      ver = true;
+    } else {
+      ver = false;
+      Serial.print("Esq: ");
+      Serial.print(m_esq);
+      Serial.print("(");
+      Serial.print(analogRead(esq));
+      Serial.print(") / Dir: ");
+      Serial.print(m_dir);
+      Serial.print("(");
+      Serial.print(analogRead(dir));
+      Serial.println(")");
 
-                delay(3000);
+      display.setCursor(0, 0);
+      display.print("Esq: ");
+      display.print(m_esq);
+      display.print("(");
+      display.print(analogRead(esq));
+      display.println(")");
 
-                dir_90(); // virar a esquerda; antes era encruzilhada();
-            }
-        }
-        else if ((leitura == 0b1001) || (leitura == 0b1111) || (leitura == 0b1011) || (leitura == 0b1101)) // ENCRUZILHADA
-        {
-            if (ver == false)
-            {
-                mot1_par();
-                mot2_par();
-                digitalWrite(led_g, 1);
-                delay(1000);
-                sensi();
-                digitalWrite(led_g, 0);
-                ver = true;
-            }
-            else
-            {
-                ver = false;
-                Serial.print("Esq: ");
-                Serial.print(m_esq);
-                Serial.print("(");
-                Serial.print(analogRead(esq));
-                Serial.print(") / Dir: ");
-                Serial.print(m_dir);
-                Serial.print("(");
-                Serial.print(analogRead(dir));
-                Serial.println(")");
+      display.print("Dir: ");
+      display.print(m_dir);
+      display.print("(");
+      display.print(analogRead(dir));
+      display.println(")");
 
-                display.setCursor(0, 0);
-                display.print("Esq: ");
-                display.print(m_esq);
-                display.print("(");
-                display.print(analogRead(esq));
-                display.println(")");
+      display.println("lei == 0001 / Dir_90");
+      display.display();
 
-                display.print("Dir: ");
-                display.print(m_dir);
-                display.print("(");
-                display.print(analogRead(dir));
-                display.println(")");
+      delay(3000);
 
-                display.println("lei == 0b1111 / Encruzi");
-                display.display();
+      dir_90();  // virar a esquerda; antes era encruzilhada();
+    }
+  } else if ((leitura == 0b1001) || (leitura == 0b1111) || (leitura == 0b1011) || (leitura == 0b1101))  // ENCRUZILHADA
+  {
+    if (ver == false) {
+      mot1_par();
+      mot2_par();
+      digitalWrite(led_g, 1);
+      delay(1000);
+      sensi();
+      digitalWrite(led_g, 0);
+      ver = true;
+    } else {
+      ver = false;
+      Serial.print("Esq: ");
+      Serial.print(m_esq);
+      Serial.print("(");
+      Serial.print(analogRead(esq));
+      Serial.print(") / Dir: ");
+      Serial.print(m_dir);
+      Serial.print("(");
+      Serial.print(analogRead(dir));
+      Serial.println(")");
 
-                delay(3000);
+      display.setCursor(0, 0);
+      display.print("Esq: ");
+      display.print(m_esq);
+      display.print("(");
+      display.print(analogRead(esq));
+      display.println(")");
 
-                encruzilhada(); // encruzilhada
-            }
-        }
+      display.print("Dir: ");
+      display.print(m_dir);
+      display.print("(");
+      display.print(analogRead(dir));
+      display.println(")");
+
+      display.println("lei == 0b1111 / Encruzi");
+      display.display();
+
+      delay(3000);
+
+      encruzilhada();  // encruzilhada
+    }
+  }
     }
     else
     {
