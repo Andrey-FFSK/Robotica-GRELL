7a69aa81926c8242eeefafdcb5f9e206096374d8
Luck07 <airameliel@gmail.com>
2023-10-23 13:37:24 -0300

    eeprom teste

--- TJR 2023/Testes/vita/eeprom.cpp
+++ TJR 2023/Testes/vita/eeprom.cpp
@@ -19,15 +19,75 @@ enum EEPROM_C {
     L_EOF = 0xff
 };
 
+
 unsigned char EEPROM[EEPROM_S];
 int EEPROM_i = 0;
 
 void clear() {
     for(int i=0; i<EEPROM_S; i++)
-        EEPROM[i] = '0';
+        EEPROM[i] = NUL;
 }
 
-void addLn(int time, EEPROM_C code, int val, )
+int EEPROM_free() {
+    int n = 0;
+    for(int i=0; i<EEPROM_S; i++) {
+        if(EEPROM[i] != NUL)
+            n++;
+    }
+    return n;
+}
+
+void print() {
+    for(int i=0; i<EEPROM_S; i++) {
+        if(EEPROM[i] != NUL)
+            printf("%c", EEPROM[i]);
+    }
+}
+
+void addLn(unsigned long mills, EEPROM_C code, int val) {
+    int secs = mills / 1000;
+    int mins = secs / 60;
+
+    EEPROM[EEPROM_i++] = TIME_INI;
+    EEPROM[EEPROM_i++] = mins;
+    EEPROM[EEPROM_i++] = ':';
+    EEPROM[EEPROM_i++] = secs;
+    EEPROM[EEPROM_i++] = TIME_END;
+
+    EEPROM[EEPROM_i++] = code;
+    EEPROM[EEPROM_i++] = val;
+
+    EEPROM[EEPROM_i++] = NL;
+}
+
+void print_decode() {
+    for(int i=0; i<EEPROM_S; i++) {
+        switch(EEPROM[i]) {
+            case NUL: break;
+            case OBJ: printf("desviando "); break;
+            case ESQ: printf("90 esquerda "); break;
+            case DIR: printf("90 direita "); break;
+            case ENCRU: printf("encruzilhada "); break;
+            case TIME_INI: printf("[ "); break;
+            case TIME_END: printf("] "); break;
+            case NL: printf("\n"); break;
+            default: printf("%u", (int)EEPROM[i]); break;
+        }
+    }
+}
+
+/*void addLn(int time, EEPROM_C code, int val) {
+    int hour = time % 60;
+    int secs = time - 60*hour;
+
+    string code_str = "";
+    switch(code) {
+        case OBJ: code_str = "desviando"; break;
+        case ESQ: code_str = "90 esquerda"; break;
+        case DIR: code_str = "90 direita"; break;
+        case ENCRU: code_str = "encruzilhada"; break;
+    }
+}*/
 
 void writeStr(string str) {
     for(int i = 0; i< str.length(); i++) {
@@ -40,10 +100,17 @@ int main() {
 
     //setup
     clear();
-    
-    for(int i=0; i<EEPROM_S; i++) {
-        printf("%c", EEPROM[i]);
-    }
+    addLn((unsigned long)13500, OBJ, 5);
+    addLn((unsigned long)28800, ESQ, 86);
+    addLn((unsigned long)153800, DIR, 100);
+
+    cout << "dados n codificados:" << endl;
+    print();
+    cout << endl << "-------------" << endl;
+    cout << "decodificados:" << endl;
+    print_decode();
+    cout << endl << "-----------" << endl;
+    cout << "memoria usada: " << EEPROM_free << " / " << EEPROM_S << endl;
 
 
     //loop
