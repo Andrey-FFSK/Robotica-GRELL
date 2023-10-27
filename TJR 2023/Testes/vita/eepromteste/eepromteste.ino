4c9e3921a19e60b16bc9e3258032e63fa88764d3
hieshe <67671207+lolpbu@users.noreply.github.com>
2023-10-26 23:11:04 -0300

    oi

--- TJR 2023/Testes/vita/eepromteste/eepromteste.ino
+++ TJR 2023/Testes/vita/eepromteste/eepromteste.ino
@@ -14,14 +14,16 @@ unsigned int EEPROM_i = 0;
 
 void clearEEPROM() {
   for(unsigned int i=0; i < EEPROM.length(); i++)
-      EEPROM.update(i, NUL);
+    EEPROM.update(i, NUL);
 }
 
-void print_raw() {
-  for(unsigned int i=0; i<EEPROM.length(); i++) {
-    if(EEPROM.read(i) != 255);
-      Serial.print(EEPROM.read(i));
+int EEPROM_memused() {
+  int n = 0;
+  for(int i=0; i<EEPROM.length(); i++) {
+    if(EEPROM.read(i) != NUL)
+      n++;
   }
+  return n;
 }
 
 String decod_c_ord(byte inp) {
@@ -40,23 +42,30 @@ String decod_c(byte inp) {
   switch(inp) {
     case NL  : return "\n"           ; break;
     case NUL : return ""             ; break;
-    case OBJ : return "desviando "   ; break;
-    case ESQ : return "90 esquerda " ; break;
-    case DIR : return "90 direita "  ; break;
+    case OBJ : return "desviando    "; break;
+    case ESQ : return "90 esquerda  "; break;
+    case DIR : return "90 direita   "; break;
     case ENCR: return "encruzilhada "; break;
-    default  : return String(inp)    ; break;
+    default  : return "codigo n reconhecido"; break;
   }
 }
 
 String decod_c_full(byte inp) {
   switch(inp) {
-    case NL  : return "NL"           ; break;
-    case NUL : return ".."             ; break;
-    case OBJ : return "dv"   ; break;
-    case ESQ : return "eq" ; break;
-    case DIR : return "dr"  ; break;
-    case ENCR: return "en"; break;
-    default  : return String(inp)    ; break;
+    case NL  : return "NL"; break;
+    case NUL : return ".."; break;
+    case OBJ : return "DV"; break;
+    case ESQ : return "EQ"; break;
+    case DIR : return "DR"; break;
+    case ENCR: return "EN"; break;
+    default: 
+      String out = "";
+      if(inp < 0x10)
+        out += '0';
+      out += String(inp, HEX);
+      out.toUpperCase();
+      return out;
+      break;
   }
 }
 
@@ -66,7 +75,9 @@ void print_ord() {
     if(b != NUL) {
       Serial.print(i);
       Serial.print(": ");
-      Serial.print(b);
+      char buf[5];
+      sprintf(buf, "%02X", b);
+      Serial.print(buf);
       Serial.print(" / ");
       Serial.println(decod_c_ord(b));
     }
@@ -77,16 +88,43 @@ void print_decod() {
   for(int i=0; i<EEPROM.length();) {
     if(EEPROM.read(i) != NUL) {
       Serial.print("[");
-      Serial.print(decod_c(EEPROM.read(i++)));
+
+      if(EEPROM.read(i) < 10) Serial.print("0");
+      Serial.print(EEPROM.read(i++)); //min
+
       Serial.print(":");
-      Serial.print(decod_c(EEPROM.read(i++)));
+
+      if(EEPROM.read(i) < 10) Serial.print("0");
+      Serial.print(EEPROM.read(i++)); //sec
+
       Serial.print("] ");
-      Serial.print(decod_c(EEPROM.read(i++)));
-      Serial.print(EEPROM.read(i++));
-      Serial.print(decod_c(EEPROM.read(i++)));
+      Serial.print(decod_c(EEPROM.read(i++))); //c
+      Serial.print(EEPROM.read(i++)); //val
+      Serial.print(decod_c(EEPROM.read(i++))); //nl
     }else{
       i++;
     }
+  }
+}
+
+void print_full() {
+  for(int i=0; i<EEPROM.length();) {
+    
+    int tmp_i = i;
+    for(int j=0; j<8; j++) {
+      char buf[5];
+      sprintf(buf, "%02X", EEPROM.read(i++));
+      Serial.print(buf);
+      Serial.print(" ");
+    }
+    Serial.print(" / ");
+    i = tmp_i;
+    for(int j=0; j<8; j++) {
+      Serial.print(decod_c_full(EEPROM.read(i++)));
+      Serial.print(" ");
+    }
+
+    Serial.println("");
   }
 }
 
@@ -102,44 +140,25 @@ void addLn(unsigned long mills, EEPROM_CODE code, int val) {
   EEPROM.write(EEPROM_i++, NL);
 }
 
-void print_full() {
-  for(int i=0; i<EEPROM.length();) {
-    
-    int tmp_i = i;
-    for(int j=0; j<8; j++) {
-      Serial.print(EEPROM.read(i++), HEX);
-      Serial.print(" ");
-    }
-    Serial.print(" / ");
-    i = tmp_i;
-    for(int j=0; j<8; j++) {
-      Serial.print(decod_c_full(EEPROM.read(i++)));
-      Serial.print(" ");
-    }
-
-    Serial.println("");
-  }
-}
-
 void setup() {
   Serial.begin(9600);
   clearEEPROM();
 
-  addLn(13500, OBJ, 5);
-  addLn(28698, ESQ, 86);
-  addLn(32213423, DIR, 100);
-  
-  Serial.println("dados codificados:");
-  print_ord();
-  Serial.println("--------------");
+  addLn(30000, OBJ, 5);
+  addLn(60000, ESQ, 86);
+  addLn(90000, DIR, 100);
+  addLn(120000, ENCR, 69);
 
   Serial.println("dados decodificados:");
   print_decod();
   Serial.println("--------------");
   
+  Serial.print("memoria usada: ");
+  Serial.print(EEPROM_memused());
+  Serial.print(" / ");
+  Serial.println(EEPROM.length());
   print_full();
   Serial.println("--------------");
-
 }
 
 void loop(){}
